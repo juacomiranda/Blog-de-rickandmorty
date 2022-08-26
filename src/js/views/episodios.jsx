5eecdf17828f889006d638c57c3a00d4facd7e59
juacomiranda <jmiranda@live.cl>
2022-08-26 14:23:25 +0000

    Link

--- src/js/views/episodios.jsx
+++ src/js/views/episodios.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "../../styles/home.css";
-import { Card } from '../component/card.jsx';
 import { Navbar } from '../component/navbar.js'
 
 export const Episodios = ()=> {
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import rigoImg from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import {Link} from "react-router-dom";
 
 export const Home = () => (
 	<div className="container">	
-			<img src="https://larepublica.pe/resizer/J2mdTSDMQAesZlmL-iwZYieskUg=/1250x735/top/smart/arc-anglerfish-arc2-prod-gruporepublica.s3.amazonaws.com/public/ONQHIVFLKVAKDKQC5WEQA4MMMI.jpg" 
+			<Link to='/personajes'>
+				<img src="https://larepublica.pe/resizer/J2mdTSDMQAesZlmL-iwZYieskUg=/1250x735/top/smart/arc-anglerfish-arc2-prod-gruporepublica.s3.amazonaws.com/public/ONQHIVFLKVAKDKQC5WEQA4MMMI.jpg" 
 					className="rounded float-start img_home" alt="..." />
-			<img src="https://blog.personal.com.py/wp-content/uploads/2020/09/los-3-mejores-episodios-de-Rick-y-Morty.jpg" 
+			</Link>
+			<Link to='/episodios'>
+				<img src="https://blog.personal.com.py/wp-content/uploads/2020/09/los-3-mejores-episodios-de-Rick-y-Morty.jpg" 
 					className="rounded float-end img_home" alt="..." />
+			</Link>	
 	</div>		
+
 	
 
 );
